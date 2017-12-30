707b7fa671ca47d662956e88a570cf6b217396c8
javiergodas <javiergodas@gmail.com>
2017-12-30 12:51:28 +0000

    Desarrollando el PVP online

--- core/src/com/atarion/game/interfaz/escena/online/EscenaPVP.java
+++ core/src/com/atarion/game/interfaz/escena/online/EscenaPVP.java
@@ -12,21 +12,16 @@ public class EscenaPVP extends EscenaOnline
     public void show()
     {
         super.show();
-        
-        if(humano != null)
-        {
-            humano = new Guardian(genesis,true);
-            humano.setX(800 / 2 - 64 / 2);
-            humano.setY(60);
-        }
-        else
-        {
-            humano2 = new Guardian(genesis,true);
-            humano2.setX(800 / 2 - 64 / 2);
-            humano2.setY(30);
+
+        humano = new Guardian(genesis,true);
+        humano.setX(800 / 2 - 64 / 2);
+        humano.setY(60);
+
+        humano2 = new Guardian(genesis,true);
+        humano2.setX(800 / 2 - 64 / 2);
+        humano2.setY(30);
             
-            humano.agregarEnemigo(humano2);
-            humano2.agregarEnemigo(humano);
-        }
+        humano.agregarEnemigo(humano2);
+        humano2.agregarEnemigo(humano);
     }
 }
