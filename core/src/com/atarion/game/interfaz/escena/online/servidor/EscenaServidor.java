e767268b0fcc110993d04bd5c725fee698915478
javiergodas <javiergodas@gmail.com>
2018-01-01 16:10:29 +0000

    Ya funciona la máquina de estados

--- core/src/com/atarion/game/interfaz/escena/online/servidor/EscenaServidor.java
+++ core/src/com/atarion/game/interfaz/escena/online/servidor/EscenaServidor.java
@@ -4,13 +4,16 @@ package com.atarion.game.interfaz.escena.online.servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.HashMap;
+import java.util.concurrent.Semaphore;
 
 
-public final class EscenaServidor implements Runnable
+public final class EscenaServidor extends Thread
 {
-    private HashMap<Integer,Socket> clientes = new HashMap<Integer,Socket>();
-    private Integer cuenta = 0, capacidad = 1;
+    private HashMap<Integer,SimpleEntry<Socket,String>> clientes = new HashMap<Integer,SimpleEntry<Socket,String>>();
+    private Integer cuenta = 0, capacidad = 2;
+    private Semaphore semaforo = new Semaphore(1,true);
 
     
     @Override
@@ -25,13 +28,19 @@ public final class EscenaServidor implements Runnable
                 System.out.println("Esperando clientes...");
                 cuenta++;
                 
-                clientes.put(cuenta,servidor.accept());
-                System.out.println("Cliente conectado: " + clientes.get(cuenta).getInetAddress());
+                clientes.put(cuenta,new SimpleEntry<>(servidor.accept(),"{}"));
+                System.out.println("Cliente conectado: " + clientes.get(cuenta).getKey().getInetAddress());
             }
             
-            clientes.entrySet().forEach(cliente -> { new HiloServidor(cliente.getValue(),cliente.getKey(),clientes).run(); } );
+            clientes.entrySet().forEach(cliente -> { new HiloServidor(cliente,this).start(); } );
         } 
         catch (IOException ex)
         {}
     }
+
+    
+    public HashMap<Integer, SimpleEntry<Socket, String>> getClientes()
+    { return clientes; }
+    public Semaphore getSemaforo()
+    { return semaforo; }
 }
--- desktop/src/com/atarion/game/desktop/ServerLauncher.java
+++ desktop/src/com/atarion/game/desktop/ServerLauncher.java
@@ -9,6 +9,6 @@ public class ServerLauncher
     public static void main(String [] args)
     {   
         System.out.println("SERVER MODE");
-        new EscenaServidor().run();
+        new EscenaServidor().start();
     }
 }
